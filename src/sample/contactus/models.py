a5207c1d407d979ca1b079fc46d68cf922bd52d9
KimJohn Quinn <kjq@logicdrop.com>
2012-03-22 12:27:18 -0400

    Added dynamic models for page data

--- src/sample/contactus/models.py
+++ src/sample/contactus/models.py
@@ -3,3 +3,14 @@
 ------------------------------
 
 """
+from piano.resources import documents as d
+
+class PageModel(d.PageData):
+    structure = {
+        'some_message':unicode,
+        'some_image':str
+    }
+    default_values = {
+        'some_message':'goodbye world!',
+        'some_image':'another_image.jpg'
+    }
