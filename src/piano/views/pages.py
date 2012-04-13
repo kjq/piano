3a6ef86124172540535e643f0e1d9f6429872a9c
KimJohn Quinn <kjq@logicdrop.com>
2012-04-13 13:20:06 -0400

    Fixed bug with save

--- src/piano/views/pages.py
+++ src/piano/views/pages.py
@@ -47,7 +47,7 @@ def edit_page(context, request):
     # Handle submission
     if 'form.submitted' in request.params:
         data = mvc.merge(request.POST.items())
-        context.save(data)
+        context.update(data)
         return HTTPFound(location=request.resource_url(context))
     # Respond
     return render_to_response(
